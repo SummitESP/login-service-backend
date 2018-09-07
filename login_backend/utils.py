5056fbe6aca4237e3e3507d22c8c821736938f79
travisjo <tjohnson@summitesp.com>
2018-09-07 15:11:52 -0500

    Working around missing import_string in Django < 1.7

--- login_backend/utils.py
+++ login_backend/utils.py
@@ -2,8 +2,13 @@
 from __future__ import unicode_literals
 
 from django.contrib.auth.models import AnonymousUser
-from django.utils.module_loading import import_string
 from django.conf import settings
+
+try:
+    from django.utils.module_loading import import_string
+except ImportError:
+    # Django < 1.7
+    from django.utils.module_loading import import_by_path as import_string
 
 
 def get_login_user(user_data):
